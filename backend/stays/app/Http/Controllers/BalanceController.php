8fa4a911579f59d929f6181da2a150ab6c210cdb
Baldomero Cho <don.baldo@hotmail.com>
2023-11-30 06:52:16 -0600

    balance

---
+++ backend/stays/app/Http/Controllers/BalanceController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\GetPrices;
+use App\Models\V1\Activity;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+class BalanceController extends Controller
+{
+    public function diff($plate){
+
+        $entrada = Activity::where('plate', $plate)
+            ->where('type', 'in')
+            ->where('status', 'in_progress')
+            ->orderBy('created_at', 'desc')
+            ->first();
+
+        $salida = Activity::where('plate', $plate)
+            ->where('type', 'out')
+            ->where('status', 'in_progress')
+            ->orderBy('created_at', 'desc')
+            ->first();
+
+        if (!$entrada || !$salida) {
+            return response()->json(['message' => 'Not found activity'], 404);
+        }
+
+        $horaEntrada = Carbon::parse($entrada->created_at);
+        $horaSalida = Carbon::parse($salida->created_at);
+
+        $diferenciaEnMinutos = $horaEntrada->diffInMinutes($horaSalida);
+
+        $price = GetPrices::perPlate($plate);
+
+        $subTotal = $price * $diferenciaEnMinutos;
+        return response()->json(
+            [
+                'subtotal' => $subTotal,
+            ]
+        );
+    }
+}
--- backend/stays/routes/api.php
+++ backend/stays/routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::apiResource('activities', 'App\Http\Controllers\ActivityController');
+    Route::get('balance/{plate}', 'App\Http\Controllers\BalanceController@diff');
 });
